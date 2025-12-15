cd2b5010cd63daf6fa8223810b17b59ffd2fc554
zmor-Yihang <3049503120@qq.com>
2025-12-15 11:07:00 +0800

    2025.12.15

--- User/app/main.c
+++ User/app/main.c
@@ -12,36 +12,37 @@ int main(void)
     HAL_Init();
     clock_init();
     usart1_init();
-    
+
     printf("USART1 DMA Test Start...\r\n");
-    
+
     // 发送测试数据
-    strcpy((char*)txBuffer, "Hello from DMA!\r\n");
-    usart1_sendData(txBuffer, strlen((char*)txBuffer));
-    
+    strcpy((char *)txBuffer, "Hello from DMA!\r\n");
+    usart1_sendData(txBuffer, strlen((char *)txBuffer));
+
     while (1)
     {
-            printf("Loop iteration, rxCompleteFlag = %d, rxSize = %d\r\n", rxCompleteFlag, rxSize);
+        printf("Loop iteration, rxCompleteFlag = %d, rxSize = %d\r\n", rxCompleteFlag, rxSize);
         // 检查是否接收到数据
-        if(rxCompleteFlag)
+        if (rxCompleteFlag)
         {
-            rxCompleteFlag = 0;  // 清除接收完成标志
-            
+            rxCompleteFlag = 0; // 清除接收完成标志
+
             // 将接收到的数据复制到回传缓冲区
-            if(rxSize <= 64) {
-                memcpy(echoBuffer, (uint8_t*)rxBuffer, rxSize);
-                
+            if (rxSize <= 64)
+            {
+                memcpy(echoBuffer, (uint8_t *)rxBuffer, rxSize);
+
                 // 先回传接收到的数据
                 usart1_sendData(echoBuffer, rxSize);
-                
+
                 // 延时一小段时间确保数据发送完成
                 HAL_Delay(10);
-                
+
                 // 再发送确认信息
                 printf("\nReceived %d bytes\r\n", rxSize);
             }
         }
         printf("Waiting for data...\r\n");
-        HAL_Delay(2000);  // 简单延时
+        HAL_Delay(2000); // 简单延时
     }
 }
---
+++ User/bsp/adc.h
@@ -0,0 +1,7 @@
+#ifndef __ADC_H__
+#define __ADC_H__
+
+#include "stm32g4xx_hal.h"
+
+
+#endif /* __ADC_H__ */
